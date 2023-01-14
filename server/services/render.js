360241045448e4a5688b301e1c8979bac8efbc5b
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2023-01-14 09:46:13 +0530

    updating localhost

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 exports.homeRoutes = (req, res) => {
-     axios.get("http://localhost:3000/api/users")
+     axios.get("https://crud-app-kingsky1t.onrender.com/api/users")
           .then(function (response) {
                console.log(response.data);
                res.render("index", { users: response.data });
@@ -16,7 +16,7 @@ exports.add_user = (req, res) => {
 };
 
 exports.update_user = (req, res) => {
-     axios.get("http://localhost:3000/api/users", { params: { id: req.query.id } })
+     axios.get("https://crud-app-kingsky1t.onrender.com/api/users", { params: { id: req.query.id } })
           .then((userData) => {
                res.render("update_user", { user: userData.data });
           })
