8f19a5ec4e56b3c4eaa88170ef82f00d6e87709d
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2023-01-14 09:43:52 +0530

    updating localhost

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,7 @@
-// document.getElementById("add_user").onsubmit(event=> {
-//      alert("Form submitted Successfully");
-// })
 $("#add_user").submit(function (event) {
      alert("Data Submitted Successfully");
 });
+
 $("#update_user").submit(function (event) {
      event.preventDefault();
 
@@ -14,7 +12,7 @@ $("#update_user").submit(function (event) {
      });
 
      var request = {
-          url: `http://localhost:3000/api/users/${data.id}`,
+          url: `https://crud-app-kingsky1t.onrender.com/api/users/${data.id}`,
           method: "PUT",
           data: data,
      };
@@ -31,7 +29,7 @@ if (window.location.pathname == "/") {
           var id = $(this).attr("data-id");
 
           var request = {
-               url: `http://localhost:3000/api/users/${id}`,
+               url: `https://crud-app-kingsky1t.onrender.com/api/users/${id}`,
                method: "DELETE",
           };
 
